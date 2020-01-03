29717295cd8200aabc8b3f0f24a46af433152247
Scott Hoisington <hoyzington@gmail.com>
2020-01-03 03:10:19 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,5 +15,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  phone.match?(/\d{7}|\(\d{3}\)\d{3}\s?-?\d{4}|(\d{3}\s?-?){2}\d{4}/)
+  phone.match?(/\A(\d{10}|\(\d{3}\)\d{3}\s?-?\d{4}|(\d{3}\s?-?){2}\d{4})\z/)
 end
