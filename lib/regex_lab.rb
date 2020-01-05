c1d7f4e49e3677784203288613017efbb203b494
Mikael Kaya <kayaswork@gmail.com>
2020-01-05 22:19:27 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,13 @@
 def starts_with_a_vowel?(word)
-
+  return text.match /[aeiou]{2}/
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  return text.scan(/un\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+  return text.match(words_five_letters_long{5,})
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
