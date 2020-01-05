4f2b3923ac556c0b004ca4d5b8a554548e81a86d
Joanna E K <kalemajoanna@gmail.com>
2020-01-05 23:29:26 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,8 +1,21 @@
+require "pry"
 def starts_with_a_vowel?(word)
+  
+# word.match?("b,c,d,f,g,h,j,k,l,m,n,p,q,r,s,t,v,w,x,y,z")
+# # "aieou".match?(/word/)
+word.match?("a,i,e,o,u")
 
-end
+
+
+
+ end 
+
 
 def words_starting_with_un_and_ending_with_ing(text)
+text.scan(/\b[Uu]n[a-z]*ing\b/)
+# .scan(/["Un"]+["ing"]/)
+
+binding.pry 
 
 end
 
@@ -15,5 +28,6 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
+ 
 
 end
