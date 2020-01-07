1fb301ba9c9fc8fa4210fd5a145c7fc915a31875
Joanna E K <kalemajoanna@gmail.com>
2020-01-07 09:39:39 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,33 +1,28 @@
 require "pry"
 def starts_with_a_vowel?(word)
   
-# word.match?("b,c,d,f,g,h,j,k,l,m,n,p,q,r,s,t,v,w,x,y,z")
-# # "aieou".match?(/word/)
-word.match?("a,i,e,o,u")
-
-
-
+# word.match?(/[a,i,e,o,u]/)
+word.match?(/^[AIEOUaieou]\w/) 
 
  end 
 
 
 def words_starting_with_un_and_ending_with_ing(text)
-text.scan(/\b[Uu]n[a-z]*ing\b/)
-# .scan(/["Un"]+["ing"]/)
-
-binding.pry 
-
+text.scan(/\b[Uu][a-z]*ing\b/)
 end
 
 def words_five_letters_long(text)
-
+ text.scan(/\b\w{5}\b/)
+  
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
+  text.match?(/^[A-Z].*\W$/)
 
 end
 
 def valid_phone_number?(phone)
- 
-
+  # phone.match?(/([0-9]) {10}/\w/)
+# binding.pry 
+phone.match(/([0-9] ?){10}/) || phone.match(/(\([0-9]{3}\)([0-9]{3}-[0-9]{4})\b)/) || phone.match(/\b([0-9]{7})\b/)
 end
